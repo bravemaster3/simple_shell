b795285c6d2e229ef62a88d94cbf406fdbe52c38
Epaphradito Lugayavu <digitomuv@gmail.com>
2023-08-13 03:21:32 +0300

    Getting Started on 4 and 4

---
+++ com_builtins.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * builtins - handling built commands
+ * @tokens: array of tokens
+ * @n_tok: number of tokens
+ * @buffer: the buffer
+ * Return: nothing
+ */
+void builtins(char **tokens, int n_tok, char *buffer)
+{
+	char *cmd = tokens[0];
+
+	if (cmd == "exit")
+	{
+
+	} else if (cmd == "env")
+	{
+
+	} else if (cmd == "cd")
+	{
+
+	} else
+	{
+
+	}
+}
